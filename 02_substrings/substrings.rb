505341f320fcfb98c514d38c45afe532de8d226d
austin-showen <austin.showen@gmail.com>
2022-04-11 19:31:56 -0700

    Add second test case and identify enumeration bug

--- 02_substrings/substrings.rb
+++ 02_substrings/substrings.rb
@@ -1,6 +1,8 @@
-def substrings(word, dictionary)
+def substrings(string, dictionary)
+  string = string.downcase
+  puts string
   result = dictionary.reduce(Hash.new(0)) do |hash, entry|
-    hash[entry] += 1 if word.include?(entry)
+    hash[entry] += 1 if string.include?(entry)
     hash
   end
   puts result
@@ -9,3 +11,8 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substrings("below", dictionary)
 # Expected output: { "below" => 1, "low" => 1 }
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# Expected output: { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+# To do: correct logic to count multiple occurrences of substrings
