a25b57715dc1d523e35ff942babb187d4f7cef68
austin-showen <austin.showen@gmail.com>
2022-04-11 18:52:28 -0700

    Create empty method and add test case

---
+++ 02_substrings/substrings.rb
@@ -0,0 +1,6 @@
+def substrings(word, dictionary)
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
+# Expected output: { "below" => 1, "low" => 1 }
