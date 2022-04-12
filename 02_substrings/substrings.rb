ccf37c0c0c5c036bfb10f2a150a5b4c32ebb8b7a
austin-showen <austin.showen@gmail.com>
2022-04-11 19:56:58 -0700

    Fix bug by using #scan instead of #include?

--- 02_substrings/substrings.rb
+++ 02_substrings/substrings.rb
@@ -1,18 +1,19 @@
 def substrings(string, dictionary)
-  string = string.downcase
-  puts string
-  result = dictionary.reduce(Hash.new(0)) do |hash, entry|
-    hash[entry] += 1 if string.include?(entry)
+  lower_string = string.downcase
+  result = dictionary.reduce(Hash.new(0)) do |hash, word|
+    matches = lower_string.scan(word)
+    num_matches = matches.length
+    hash[word] = matches.length if matches.length > 0
     hash
   end
-  puts result
+  result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+puts substrings("below", dictionary)
 # Expected output: { "below" => 1, "low" => 1 }
 
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
 # Expected output: { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
 
 # To do: correct logic to count multiple occurrences of substrings
