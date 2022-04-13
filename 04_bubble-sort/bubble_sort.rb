60b8fca1fb257c866b413928036a8b43947b7a02
austin-showen <austin.showen@gmail.com>
2022-04-13 16:48:47 -0700

    Refactor for style and clarity

--- 04_bubble-sort/bubble_sort.rb
+++ 04_bubble-sort/bubble_sort.rb
@@ -1,24 +1,19 @@
 def bubble_sort(list)
   sorted = false
 
-  while sorted == false do
+  while sorted == false
     # Will remain true only if no numbers are swapped
     sorted = true
-    list.each_with_index do |number, index|
-      # Avoid nil comparisons by stopping at length-1
-      unless index == list.length-1
-        if list[index] > list[index+1]
-          # At least one pair of numbers was unordered, so remain in the while loop
-          sorted = false
-          # Swap unordered pair
-          list[index], list[index+1] = list[index+1], list[index]
-        end
-      end
+    list[0..-2].each_with_index do |_value, i|
+      next if list[i] <= list[i + 1]
+
+      # At least one pair of numbers was unordered, so swap them and remain in the while loop
+      sorted = false
+      list[i], list[i + 1] = list[i + 1], list[i]
     end
   end
-
   list
 end
 
-print(bubble_sort([4,3,78,2,0,2]), "\n")
-# Expected output: [0,2,2,3,4,78]
+print(bubble_sort([4, 3, 78, 2, 0, 2]), "\n")
+# Expected output: [0, 2, 2, 3, 4, 78]
