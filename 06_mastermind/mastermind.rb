78470bf053e01b5add4db4d069639d095fea2f71
austin-showen <austin.showen@gmail.com>
2022-04-19 19:26:48 -0700

    Refactor evaluate_guess method for clarity

--- 06_mastermind/mastermind.rb
+++ 06_mastermind/mastermind.rb
@@ -4,25 +4,27 @@ class Game
     @codebreaker = codebreaker
     @guesses = 1
     @code_length = 4
-    @colors = ['R', 'O', 'Y', 'G', 'B', 'V']
+    @colors = %w[R O Y G B V]
     play
   end
 
   def play
     @code = @codemaker.generate_code(@code_length, @colors)
     @guess = @codebreaker.guess(@guesses)
-    @score = @codemaker.evaluate_guess(@code, @guess)
+    @score = @codemaker.evaluate_guess(@guess)
+    p @score
     @guesses += 1
   end
-
 end
 
 class ComputerCodemaker
   def initialize
+    @code = ''
+    @exact = 0
+    @inexact = 0
   end
 
   def generate_code(code_length, colors)
-    @code = ""
     code_length.times do
       @code += colors[rand(6)]
     end
@@ -30,23 +32,31 @@ class ComputerCodemaker
     @code
   end
 
-  def evaluate_guess(code, guess)
-    @exact = 0
-    @inexact = 0
-    for i in (0..3) do
-      if code[i] == guess[i]
+  def evaluate_guess(guess)
+    @guess = guess
+    exact_matches
+    inexact_matches
+    { exact: @exact, inexact: @inexact }
+  end
+
+  def exact_matches
+    @code.each_char.with_index do |char, i|
+      if char == @guess[i]
         @exact += 1
-        code[i], guess[i] = 'X', 'Z'
+        @guess[i] = 'Z'
+        @code[i] = 'X'
       end
     end
-    for i in (0..3) do
-      if guess.include? code[i]
+  end
+
+  def inexact_matches
+    @code.each_char.with_index do |char, i|
+      if @guess.include? char
         @inexact += 1
-        guess.sub!(code[i], 'Z')
-        code[i] = 'X'
+        @guess.sub!(char, 'Z')
+        @code[i] = 'X'
       end
     end
-    {exact: @exact, inexact: @inexact}
   end
 end
 
@@ -70,11 +80,11 @@ class PlayerCodebreaker
 
   def guess(guesses)
     puts "Guess ##{guesses} out of 12"
-    puts "Please enter your guess as a series of four letters, such as RRGV."
-    puts "The colors are (R)ed, (O)range, (Y)ellow, (G)reen, (B)lue, and (V)iolet."
+    puts 'Please enter your guess as a series of four letters, such as RRGV.'
+    puts 'The colors are (R)ed, (O)range, (Y)ellow, (G)reen, (B)lue, and (V)iolet.'
     guess = gets.chomp.upcase
     until valid?(guess)
-      puts "Invalid guess! Try again."
+      puts 'Invalid guess! Try again.'
       guess = gets.chomp.upcase
     end
     guess
@@ -82,11 +92,12 @@ class PlayerCodebreaker
 
   def valid?(guess)
     return false unless guess.length == 4
+
     guess.each_char { |char| return false unless 'ROYGBV'.include?(char) }
-    return true
+    true
   end
 end
 
 codemaker = ComputerCodemaker.new
 codebreaker = PlayerCodebreaker.new
-game = Game.new(codemaker, codebreaker)
+Game.new(codemaker, codebreaker)
