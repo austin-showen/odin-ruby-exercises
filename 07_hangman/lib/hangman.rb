ac3e11654cd5e3409f17f7f7e6dff649e3f06bc8
austin-showen <austin.showen@gmail.com>
2022-05-19 13:08:50 -0700

    Refactor into a Game class

--- 07_hangman/lib/hangman.rb
+++ 07_hangman/lib/hangman.rb
@@ -1,25 +1,38 @@
-def clean_dictionary(dictionary)
-  dictionary.each(&:chomp!)
-  dictionary.select! { |word| word if word.length >= 5 && word.length <= 12 }
-end
+class Game
+  def initialize
+    @dictionary = File.open('dictionary.txt', 'r').readlines
+    clean_dictionary
+    @word = @dictionary[rand(@dictionary.length)].upcase
+    @remaining_guesses = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    @blanks = '_' * @word.length
+    get_indices(@word)
+    player_guess
+    process_guess
+  end
 
-def word_indices(word)
-  indices = {}
-  word = word.split('')
-  word.each_with_index do |char, i|
-    if indices.include?(char)
-      indices[char].push(i)
-    else
-      indices[char] = [i]
+  def clean_dictionary
+    @dictionary.each(&:chomp!)
+    @dictionary.select! { |word| word if word.length >= 5 && word.length <= 12 }
+  end
+
+  def get_indices(word)
+    @indices = {}
+    word = word.split('')
+    word.each_with_index do |char, i|
+      @indices.include?(char) ? @indices[char].push(i) : @indices[char] = [i]
     end
   end
-  indices
+
+  def player_guess
+    @guess = ''
+    @guess = gets.chomp.upcase until @guess.length == 1 && @remaining_guesses.include?(@guess)
+  end
+
+  def process_guess
+    @remaining_guesses.delete(@guess)
+    puts 'yay' if @indices.include?(@guess)
+  end
 end
 
-dictionary = File.open('dictionary.txt', 'r').readlines
-dictionary = clean_dictionary(dictionary)
-word = dictionary[rand(dictionary.length)]
-puts word
-
-indices = word_indices(word)
-puts indices
+game = Game.new
+game
